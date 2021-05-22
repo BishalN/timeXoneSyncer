20ca18799a961229e92207c445f28e540443884c
Bishal Neupane <neupanebishal07@gmail.com>
2021-05-22 16:38:51 +0545

    Next Tailwind Storybook setup

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: [
+    // Paths to the story files
+    '../pages/*.stories.mdx',
+    '../pages/*.stories.js',
+    '../pages/index.story.js',
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+};
