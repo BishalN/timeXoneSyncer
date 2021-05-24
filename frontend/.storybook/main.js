5bd6b0f22337a57dac122eae0f285348868cf1ca
Bishal Neupane <neupanebishal07@gmail.com>
2021-05-24 08:45:09 +0545

    Typescript setup Next and Story

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -6,4 +6,13 @@ module.exports = {
     '../pages/index.story.js',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
 };
