0868e3c6416d80a77c213c0a629d31f09992e3a5
Bishal Neupane <neupanebishal07@gmail.com>
2021-05-28 17:12:27 +0545

    Grid Vs flex Test

--- frontend/src/pages/play.tsx
+++ frontend/src/pages/play.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { HeroSVGMobile } from "../illustrations/Hero";
+import { MainCTA } from "../ui/components/MainCTA";
 
 interface playProps {}
 
-export const play: React.FC<playProps> = ({}) => {
-  return <h1>Hello</h1>;
+const play: React.FC<playProps> = ({}) => {
+  return (
+    <section id="HeroSection" className="container mx-auto">
+      <div
+        id="sectionWrapper"
+        className="grid grid-cols-1 md:grid-cols-2 justify-items-center"
+      >
+        <HeroSVGMobile />
+
+        <div className="mx-2 sm:my-16 sm:space-y-4 lg:space-y-5 lg:mr-32 lg:mt-40">
+          <h1 className="text-secondary italic text-4xl sm:text-5xl lg:text-6xl">
+            TimeXoneSyncer
+          </h1>
+          <p className="text-xl text-primary-300 my-2 max-w-sm">
+            Lets you sync your time with anyone’s time in the world with a click
+            of a button
+          </p>
+          <MainCTA />
+        </div>
+      </div>
+    </section>
+  );
 };
+
+export default play;
